050b5d04b174bfc795e7e94aef9db0fdac3123d3
Suresh kanzariya <suresh.kanzariya@creditt.in>
2026-06-15 18:36:24 +0530

    Add new contectus component

---
+++ src/app/contactus/contactus.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contactus',
+  standalone: true,
+  templateUrl: './contactus.component.html',
+  styleUrls: ['./contactus.component.css']
+})
+export class ContactUsComponent {}
