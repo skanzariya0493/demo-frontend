0913624264aaff91ea25e1a0e2dc66ed778e8223
Suresh kanzariya <suresh.kanzariya@creditt.in>
2026-06-15 18:52:59 +0530

    Add new contectus component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
-import { ContactUsComponent } from './contactus/contactus.component';
+import { AboutComponent } from './component/about/about.component';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'home', pathMatch: 'full' },
 	{ path: 'home', component: HomeComponent },
-	{ path: 'contact', component: ContactUsComponent },
+	{ path: 'about', component: AboutComponent },
 	{ path: '**', redirectTo: 'home' },
 ];
--- src/app/contactus/contactus.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-contactus',
-  standalone: true,
-  templateUrl: './contactus.component.html',
-  styleUrls: ['./contactus.component.css']
-})
-export class ContactUsComponent {}
